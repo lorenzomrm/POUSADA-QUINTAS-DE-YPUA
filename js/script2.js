4d2887e93db0c2237db29ef265f045d6112f598b
lorenzo <lorenzo.mr.rm@gmail.com>
2024-11-25 10:49:08 -0300

    Ajustes 2.0  Links

--- js/script2.js
+++ js/script2.js
@@ -54,7 +54,7 @@ document.getElementById('form-edicao').addEventListener('submit', function (even
         alerta.style.display = 'block';
         setTimeout(function() {
             alerta.style.display = 'none';
-            window.location.href = 'MenuPrincipal.html';
+            window.location.href = 'Acomodacoes.html';
         }, 3000);
     }
 });
